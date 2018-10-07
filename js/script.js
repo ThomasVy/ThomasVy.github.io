d9eb367a1b1ebaeace49049f35b3f19e19881f1f
ThomasVy <vythomas97@gmail.com>
2018-10-07 16:19:08 -0600

    add some images for links

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function bringUpWindow(){
+				window.alert("Not yet implemented.");
+	}
+ function MyFunction(){
+ 	document.getElementById("demo").innerHTML = "Hello World";
+ }
+ function MyFunction2(){
+ 	var x, y;
+ 	x =2; y = 5;
+ 	return x+y;
+ }
